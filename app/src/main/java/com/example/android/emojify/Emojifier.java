7258a0cfcd5b3831a664dad254b4e3bcd2f184f0
Nikita Gamolsky <nikita.gamolsky@gmail.com>
2017-01-24 14:28:47 -0800

    TLIB.04-Exercise-MatchFaceToEmoji

--- app/src/main/java/com/example/android/emojify/Emojifier.java
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -62,6 +62,7 @@ class Emojifier {
 
                 // Log the classification probabilities for each face.
                 getClassifications(face);
+                // TODO (6): Change the call to getClassifications to whichEmoji() to log the appropriate emoji for the facial expression.
             }
 
         }
@@ -78,11 +79,19 @@ class Emojifier {
      * @param face The face to get the classification probabilities.
      */
     private static void getClassifications(Face face){
+        // TODO (2): Change the name of the getClassifications() method to whichEmoji() (also change the log statements)
         // Log all the probabilities
         Log.d(LOG_TAG, "getClassifications: smilingProb = " + face.getIsSmilingProbability());
         Log.d(LOG_TAG, "getClassifications: leftEyeOpenProb = "
                 + face.getIsLeftEyeOpenProbability());
         Log.d(LOG_TAG, "getClassifications: rightEyeOpenProb = "
                 + face.getIsRightEyeOpenProbability());
+
+        // TODO (3): Create threshold constants for a person smiling, and and eye being open by taking pictures of yourself and your friends and noting the logs.
+        // TODO (4): Create 3 boolean variables to track the state of the facial expression based on the thresholds you set in the previous step: smiling, left eye closed, right eye closed.
+        // TODO (5): Create an if/else system that selects the appropriate emoji based on the above booleans and log the result.
     }
+
+
+    // TODO (1): Create an enum class called Emoji that contains all the possible emoji you can make (smiling, frowning, left wink, right wink, left wink frowning, right wink frowning, closed eye smiling, close eye frowning).
 }
